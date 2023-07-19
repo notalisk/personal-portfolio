050e2a971c3853cddfd8c55ec6fbb4ae0ef48f41
Ellen Kolbly <81662512+notalisk@users.noreply.github.com>
2023-07-18 23:28:42 -0500

    re-upload template

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,13 +1,43 @@
-## Setup the Project
+Contributing Guidelines
 
-The following steps will get you up and running to contribute to this repository:
+Thank you for your interest in contributing to our project! We welcome contributions from everyone. By participating in this project, you agree to abide by the following guidelines:
 
-1. Fork the repo (click the Fork button at the top right of this page)
-2. Clone your fork locally
+Code of Conduct:
+Please review and adhere to our Code of Conduct, which establishes a welcoming and inclusive environment for all contributors. 
 
-   ```bash
-   git clone https://github.com/<your_github_username>/enji.dev.git
-   cd enji.dev
-   ```
+Types of Contributions:
+We welcome different types of contributions, including but not limited to:
 
-3. Setup all the dependencies and packages by running `pnpm install`
+- Bug fixes
+- New features or enhancements
+- Documentation improvements
+- Test cases
+- Translations
+- Feedback and suggestions
+
+Getting Started:
+To get started with contributing, follow these steps:
+
+1. Fork the repository and clone it to your local machine.
+2. Install the necessary dependencies.
+3. Create a new branch for your contribution.
+4. Make your changes or additions.
+5. Run tests and ensure that everything is working as expected.
+6. Commit your changes with a clear and descriptive commit message.
+7. Push your changes to your forked repository.
+8. Submit a pull request (PR) to our main repository, clearly explaining the purpose and details of your contribution.
+
+Code Style and Standards:
+Please ensure that your code adheres to our coding style and standards. Review our style guide and follow the established conventions for formatting, naming, and documentation.
+
+Documentation:
+Update relevant documentation to reflect your changes. This includes README files, code comments, and any other supporting documentation.
+
+Issue Reporting:
+If you encounter any bugs or issues, please submit a detailed bug report, including steps to reproduce and any relevant information. You can also search for existing issues and contribute to their resolution.
+
+Community Feedback:
+We value feedback and suggestions from the community. Join our discussion forums, participate in conversations, and share your ideas for improving the project.
+
+Thank you for your contributions and for helping make this project better!
+
---
+++ src/data/user.js
@@ -0,0 +1,74 @@
+const INFO = {
+	main: {
+		title: "Ellen K - Portfolio",
+		name: "Ellen K",
+		email: "ellen@rscheme.org",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/notalisk",
+		linkedin: "https://www.linkedin.com/in/ellen-kolbly/",
+	},
+
+	homepage: {
+		title: "Full-stack web developer, and cat enthusiast.",
+		description:
+			"I am a fullstack developer with expertise in Node.js, React,js, and Flutter. I write in JavaScript, CSS, HTML, and have a basic understand of Python and Java. I build websites that are functional and reliable, and I strive to make them look good while doing it. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Ellen Kolbly, a full stack web developer in Austin, TX.",
+		description:
+			"I've been coding since I was a child, and recently graduated from University of Texas in Austin with a certificate in Full Stack Web Development. I enjoy building websites, and I am skilled at it. I am looking for an opportunity to put my skills to good use in an environment where I will be respected and work with other respected individuals.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
